642ab204056d1b20b70e528923528b46ee7bd626
Nicole Chow <nicolechow@Nicoles-MacBook-Pro.local>
2017-11-23 12:14:12 -0800

    fixed issues with array on .post

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,16 @@
 var allFriends = [
 
 	{
+		name: "Natalia",
+		photo: "https://images.pexels.com/photos/407237/pexels-photo-407237.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+		scores: ["1","1","1","1","1","1","1","1","1","1"]
+		//10
+	},	
+
+	{
 		name: "Arnold",
 		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
-		scores: [1,2,3,5,4,5,3,2,1,2]
+		scores: ["1","4","5","2","1","3","1","2","4","5"]
 		// 28
 
 	},
@@ -11,16 +18,17 @@ var allFriends = [
 	{
 		name: "Ashley",
 		photo: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
-		scores: [2,2,4,5,4,5,3,2,1,2]
+		scores: ["1","2","1","4","4","4","3","1","2","5"]
 		//30
 	},	
 
 	{
-		name: "Natalia",
-		photo: "https://images.pexels.com/photos/407237/pexels-photo-407237.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
-		scores: [1,1,1,1,1,1,1,1,1,1]
-		//10
+		name: "Peter",
+		photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb",
+		scores: ["4","1","2","3","4","1","2","3","4","1"]
+		//30
 	},	
+
 
 
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,7 +15,8 @@ var friends = require("../data/friends")
 
 module.exports = function(app){
 	app.get("/api/allUsers", function(req,res){
-		res.json(friends);
+		res.send(friends);
+		// res.json(friends);
 	});
 
 	app.get("/api/:urlUserName", function(req,res){
@@ -38,22 +39,37 @@ module.exports = function(app){
 		bestFriend.bestFriendName = friends[0].name;
 		bestFriend.bestFriendPhoto = friends[0].photo;
 
-		var myIndex = friends.length - 1;		
-		var scoreDiff = Math.abs(totalScore(friends[0].scores) - friends[myIndex].scores);
+		var myIndex = friends.length - 1;	
+		var myScore = friends[myIndex].scores;
+		var myScoreInNum = convertStrArrToNumArr(myScore);
+		var firstUserScore = friends[0].scores; 
+		var firstUserScoreInNum = convertStrArrToNumArr(firstUserScore);
+		var lowestScoreDiff = compareScore(myScoreInNum,firstUserScoreInNum);		
+
+		console.log("lowestScoreDiff " + lowestScoreDiff);
 
 		for (var i= 1; i<myIndex; i++) {
-			var otherUserScore = friends[i].scores; 
-			var myScore = friends[myIndex].scores;	
-			var newScoreDiff = Math.abs(totalScore(otherUserScore) - myScore);
+		
+			var otherUserScore = friends[i].scores;
+			var otherUserScoreInNum = convertStrArrToNumArr(otherUserScore);
 
-			if (newScoreDiff<scoreDiff){
+			var newScoreDiff = compareScore(myScoreInNum,otherUserScoreInNum);
+			console.log("newScoreDiff " + friends[i].name + newScoreDiff);
+
+			if (newScoreDiff<lowestScoreDiff){
+				lowestScoreDiff = newScoreDiff;
 				bestFriend.bestFriendName = friends[i].name;
 				bestFriend.bestFriendPhoto = friends[i].photo;
 			}
-		}
-		res.json(bestFriend);		
-	});
 
+
+		}
+		res.json(bestFriend);	
+	});
+}
+
+function convertStrArrToNumArr(strArr) {
+	return strArr.map(Number)
 }
 
 function totalScore(arr){
@@ -62,3 +78,13 @@ function totalScore(arr){
 	}, 0);
 	return sum;
 }
+
+function compareScore(arr1,arr2){
+	var diffArr=[];
+	arr1.forEach(function(element,index){
+		var difference = Math.abs(element - arr2[index]);
+		diffArr.push(difference);
+	})
+	return totalScore(diffArr);
+}
+
